8dc413b90f52224ca0cde185130f305e2ba85a60
Jason Raimondi <jason@raimondi.us>
2015-09-02 15:38:42 -0400

    initialize frontend assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
+var paths = {
+	'BOWER': './vendor/bower_components/',
+	'DESTINATION': './public/assets/',
+	'SOURCE': './resources/assets/',
+    'NODE': './node_modules/',
+	'FOUNDATION': './node_modules/foundation-sites/',
+	'VIEWS': './resources/views/',
+}
+
 var elixir = require('laravel-elixir');
 
 /*
@@ -6,11 +15,22 @@ var elixir = require('laravel-elixir');
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we are compiling the Less
  | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    mix.copy(paths.SOURCE + 'images', paths.DESTINATION + 'images');
+    mix.copy(paths.SOURCE + 'vendor', paths.DESTINATION + 'vendor');
+
+    mix.sass('style.scss', paths.DESTINATION + 'css/style.css', {
+        includePaths: [
+            paths.FOUNDATION + 'scss'
+        ]
+    });
+
+    mix.browserify(paths.SOURCE + 'js/app.js', paths.DESTINATION + 'js/package.js', './');
+
 });
---
+++ resources/assets/js/app.js
@@ -0,0 +1,25 @@
+var paths = {
+	'BOWER': '../../vendor/bower_components/',
+	'DESTINATION': '../../public/assets/',
+	'SOURCE': '../../resources/assets/',
+    'NODE': '../../node_modules/',
+	'FOUNDATION': '../../node_modules/foundation-sites/js/foundation/',
+}
+
+var foundation  = require('foundation-sites');
+// var abide       = require(paths.FOUNDATION + 'foundation.abide');
+// var accordion   = require(paths.FOUNDATION + 'foundation.accordion');
+// var alert       = require(paths.FOUNDATION + 'foundation.alert');
+// var clearing    = require(paths.FOUNDATION + 'foundation.clearing');
+// var dropdown    = require(paths.FOUNDATION + 'foundation.dropdown');
+// var equalizer   = require(paths.FOUNDATION + 'foundation.equalizer');
+// var interchange = require(paths.FOUNDATION + 'foundation.interchange');
+// var joyride     = require(paths.FOUNDATION + 'foundation.joyride');
+// var magellan    = require(paths.FOUNDATION + 'foundation.magellan');
+// var offcanvas   = require(paths.FOUNDATION + 'foundation.offcanvas');
+// var orbit       = require(paths.FOUNDATION + 'foundation.orbit');
+// var reveal      = require(paths.FOUNDATION + 'foundation.reveal');
+// var slider      = require(paths.FOUNDATION + 'foundation.slider');
+// var tab         = require(paths.FOUNDATION + 'foundation.tab');
+// var tooltip     = require(paths.FOUNDATION + 'foundation.tooltip');
+// var topba       = require(paths.FOUNDATION + 'foundation.topbar');
