4b6d3ae86cb9677f6191d865c73dc238c296aaf6
Jason Raimondi <jason@raimondi.us>
2015-09-03 12:04:23 -0400

    add routes we are going to need

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,14 +3,15 @@
 Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+    Route::get('my-tickets', ['as' => 'my-tickets', 'uses' => 'HomeController@myTickets']);
 
 });
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::resource('schools', 'SchoolController', ['only' => ['index', 'show']]);
+Route::resource('tickets', 'TicketController', ['only' => ['index', 'show']]);
 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('auth/login',     ['as' => 'login',         'uses' => 'Auth\AuthController@getLogin']);
+Route::post('auth/login',    ['as' => 'login.post',    'uses' => 'Auth\AuthController@postLogin']);
+Route::get('auth/logout',    ['as' => 'logout',        'uses' => 'Auth\AuthController@getLogout']);
+Route::get('auth/register',  ['as' => 'register',      'uses' => 'Auth\AuthController@getRegister']);
+Route::post('auth/register', ['as' => 'register.post', 'uses' => 'Auth\AuthController@postRegister']);
